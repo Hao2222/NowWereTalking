77fc27f6b2e2a409a8e9eefb97de0763024c96ae
Maira Acevedo <m.acevedo53@yahoo.com>
2020-12-31 13:04:21 -0800

    app.js w/router

--- client/src/App.js
+++ client/src/App.js
@@ -6,9 +6,11 @@ import { BrowserRouter as Router, Route } from "react-router-dom";
 
 function App() {
   return (
+    <Router>
     <div className="App">
       <Navigation />
     </div>
+    </Router>
   );
 }
 
