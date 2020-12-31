9c3a0f4253cf62ea4874ea234664d8c6a1fe7976
Maira Acevedo <m.acevedo53@yahoo.com>
2020-12-31 13:00:55 -0800

    imported react router dom

--- client/src/components/Navigation/index.js
+++ client/src/components/Navigation/index.js
@@ -1,5 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import Navbar from "react-bootstrap/Navbar";
+import Nav from "react-bootstrap/Nav";
 
 function Navigation() {
     return (
@@ -19,3 +21,5 @@ function Navigation() {
         </Navbar>
     )
 }
+
+export default Navigation;
