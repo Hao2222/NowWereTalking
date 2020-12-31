1f48496f134773993e652da22588854fdeb072b0
Maira Acevedo <m.acevedo53@yahoo.com>
2020-12-31 12:56:50 -0800

    updated nav bar links

--- client/src/components/Navigation/index.js
+++ client/src/components/Navigation/index.js
@@ -1,19 +1,21 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-function Navigation (){
+function Navigation() {
     return (
         <Navbar bg="light" expand="lg">
-  <Navbar.Brand href="#home">Now We're Talking</Navbar.Brand>
-  <Navbar.Toggle aria-controls="basic-navbar-nav" />
-  <Navbar.Collapse id="basic-navbar-nav">
-    <Nav className="mr-auto">
-      <Link className = "nav-item nav-link" to="/">Home</Link>
-      <Link href="#link">Link</Link>
-      
-    </Nav>
-    
-  </Navbar.Collapse>
-</Navbar>
+            <Navbar.Brand href="#home">Now We're Talking</Navbar.Brand>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav">
+                <Nav className="mr-auto">
+                    <Link className="nav-item nav-link" to="/">Home</Link>
+                    <Link className="nav-item nav-link" to="/topics">Topics</Link>
+                    <Link className="nav-item nav-link" to="/profile">Profile</Link>
+                    <Link className="nav-item nav-link" to="/challenge">Challenge</Link>
+
+                </Nav>
+
+            </Navbar.Collapse>
+        </Navbar>
     )
 }
